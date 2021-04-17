ae45aecdc27f922293ff8ebd775439b047406b4f
Sentinel <75678325+sentinelb51@users.noreply.github.com>
2021-04-17 20:44:38 +0400

    Initial release

---
+++ tcp_ping.py
@@ -0,0 +1,69 @@
+from socket import inet_aton, socket, AF_INET, SOCK_STREAM, getservbyport, SHUT_RD, timeout
+from sys import argv
+from time import time_ns, sleep
+
+
+def ip_is_valid(address: str) -> bool:
+    try:
+        inet_aton(address)
+    except (OverflowError, OSError, ValueError):
+        return False
+    else:
+        return True
+
+
+def port_is_valid(port: int) -> bool:
+    try:
+        getservbyport(port)
+    except (OverflowError, OSError, ValueError):
+        return False
+    else:
+        return True
+
+
+def try_connect(ip_address, port) -> None:
+    tcp_socket = socket(AF_INET, SOCK_STREAM)
+    tcp_socket.settimeout(0.5)
+    # Re-creating a socket every time is expensive but it's to avoid WinError-10053
+    t1 = time_ns()
+    try:
+        tcp_socket.connect((ip_address, port))
+    except timeout:
+        print(f"Connection to {ip_address} timed out in {(time_ns() - t1) / 1000000}ms")
+    except OSError as exception:
+        print(f"General error when trying to connect; {exception}")
+    else:
+        print(f"Connected to {ip_address}; {(time_ns() - t1) / 1000000}ms")
+        tcp_socket.shutdown(SHUT_RD)
+
+
+def main():
+    arguments = argv
+
+    if not arguments[1:]:
+        raise AttributeError("Insufficient arguments provided")
+
+    if ":" not in arguments[1]:
+        raise AttributeError("Invalid format provided, expected IP:PORT")
+
+    ip_address, port = arguments[1].split(":")
+    if not ip_is_valid(ip_address):
+        raise TypeError("Invalid or illegal IPv4 address provided")
+
+    if not port.isdigit():
+        raise TypeError("Port provided is not an integer")
+
+    port = int(port)
+    if not port_is_valid(port):
+        raise TypeError("Invalid or illegal port provided")
+
+    try:
+        for _ in range(8):
+            try_connect(ip_address, port)
+            sleep(0.5)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
