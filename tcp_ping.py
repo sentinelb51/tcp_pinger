676f1e5ed7d71d63a3f3a926bc4f46e4fdb9f6ac
Sentinel <75678325+sentinelb51@users.noreply.github.com>
2021-04-18 16:17:35 +0400

    Update 1.1, reworked, reformatted, cleaned up

--- tcp_ping.py
+++ tcp_ping.py
@@ -1,58 +1,90 @@
-from socket import inet_aton, socket, AF_INET, SOCK_STREAM, getservbyport, SHUT_RD, timeout
-from sys import argv
+from argparse import ArgumentParser
+from ipaddress import ip_address
+from signal import signal, SIGINT
+from socket import socket, AF_INET, SOCK_STREAM, timeout, SHUT_RD
 from time import time_ns, sleep
 
+parser = ArgumentParser()
 
-def ip_is_valid(address: str) -> bool:
+parser.add_argument("host", nargs="?", help="Remote address and port")
+parser.add_argument("-n", help="Number of requests to send", type=int, default=4)
+parser.add_argument("-w", help="Delay in milliseconds to use in between requests", type=int, default=450)
+parser.add_argument("-o", help="Timeout in milliseconds to wait for each reply", type=int, default=5000)
+args = parser.parse_args()
+
+sent, dropped, latencies = 0, 0, []
+
+
+def finalise(_, __):
+    print()
+    if sent or dropped:
+        request_statistics = f"Received: {sent}/{sent + dropped} requests"
+
+        if not sent:
+            request_statistics.join(f"100% loss)")
+        else:
+            request_statistics.join(f"{round(dropped / sent, 2)}% loss)")
+        print(request_statistics)
+
+        if latencies:
+            latency_statistics = f"Min: {min(latencies)}ms, Max: {max(latencies)}ms, Avg: {round(sum(latencies) / len(latencies), 3)}ms"
+            print(latency_statistics)
+        
+    exit(0)
+
+
+signal(SIGINT, finalise)
+
+
+def input_is_valid(address: str, port: int) -> bool:
     try:
-        inet_aton(address)
+        ip_address(address)
     except (OverflowError, OSError, ValueError):
         return False
     else:
-        return True
-
-def try_connect(ip_address: str, port: int) -> None:
-    tcp_socket = socket(AF_INET, SOCK_STREAM)
-    tcp_socket.settimeout(0.5)
-    # Re-creating a socket every time is expensive but it's to avoid WinError-10053
-    t1 = time_ns()
-    try:
-        tcp_socket.connect((ip_address, port))
-    except timeout:
-        print(f"Connection to {ip_address} timed out in {(time_ns() - t1) / 1000000}ms")
-    except OSError as exception:
-        print(f"General error when trying to connect; {exception}")
-    else:
-        print(f"Connected to {ip_address}; {(time_ns() - t1) / 1000000}ms")
-        tcp_socket.shutdown(SHUT_RD)
+        return 0 < port <= 65535
 
 
 def main():
-    arguments = argv
+    if not args.host:
+        return print("Host address not provided")
 
-    if not arguments[1:]:
-        raise AttributeError("Insufficient arguments provided")
+    host = args.host.split(":")
+    if len(host) != 2:
+        return print("Invalid host:port format provided")
 
-    if ":" not in arguments[1]:
-        raise AttributeError("Invalid format provided, expected IP:PORT")
+    address, port = host[0], int(host[1])
+    if not input_is_valid(address, port):
+        return print("Host address or port range invalid")
 
-    ip_address, port = arguments[1].split(":")
-    if not ip_is_valid(ip_address):
-        raise TypeError("Invalid or illegal IPv4 address provided")
+    global sent, dropped, latencies
 
-    if not port.isdigit():
-        raise TypeError("Port provided is not an integer")
+    TIMEOUT, WAIT = args.o / 1000, args.w / 1000
 
-    port = int(port)
-    if not 0 < port <= 65535:
-        raise ValueError("Port range is out of boundaries")
+    print("\n----------- TCP Ping 1.1 -----------\n"
+          f"Initialising connection to {address}:{port}\n")
+    for sequence in range(1, args.n + 1):
+        tcp_socket = socket(AF_INET, SOCK_STREAM)
+        tcp_socket.settimeout(TIMEOUT)
+        sleep(WAIT)
 
-    try:
-        for _ in range(8):
-            try_connect(ip_address, port)
-            sleep(0.5)
-    except KeyboardInterrupt:
-        pass
+        t1 = time_ns()
+        try:
+            tcp_socket.connect((address, port))
+        except timeout:
+            print(f"Sequence {sequence}: Timed out ({args.o}ms)")
+            dropped += 1
+        except OSError as exception:
+            print(f"Sequence {sequence}: Generic error ({exception})")
+            dropped += 1
+        else:
+            latency = (time_ns() - t1) / 1000000
+            print(f"Sequence {sequence}: Connected in {latency}ms")
+            tcp_socket.shutdown(SHUT_RD)
+            sent += 1
+            latencies.append(latency)
+
+    finalise(None, None)
 
 
 if __name__ == "__main__":
